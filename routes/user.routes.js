7862a86a7775e9be8a2dfad6758b4f596cfff01d
enirevlow <foowinlin@gmail.com>
2022-08-15 07:38:28 +0800

    fix getprofile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,6 +10,6 @@ const protect = require("../middleware/auth.middleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("profile", protect, getProfile);
+router.get("/profile", protect, getProfile);
 
 module.exports = router;
