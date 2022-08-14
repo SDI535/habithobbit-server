08160962f20bef33cc0415a734e80a34b5e5ad3f
enirevlow <foowinlin@gmail.com>
2022-08-15 07:26:10 +0800

    remove auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,8 +6,6 @@ const {
   getProfile,
 } = require("../controllers/user.controller");
 
-const protect = require("../middleware/auth.middleware");
-
 router.post("/", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", getProfile);
